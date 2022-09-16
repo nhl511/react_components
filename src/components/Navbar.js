f9a8cbeffe033e485012b8e8fdb22fc56eb1304c
bestadvutru <longnguyen5112000@gmail.com>
2022-09-16 10:16:59 +0700

    Functional Components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,65 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white">
+//         <div className="container-fluid">
+//             {/* <a className="navbar-brand" href="#">Navbar</a>
+//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon"></span>
+//             </button> */}
+//             <div className="collapse navbar-collapse" id="navbarNav">
+//             <ul className="navbar-nav">
+//                 <li className="nav-item">
+//                 <a className="nav-link active" aria-current="page" href="#">Home</a>
+//                 </li>
+//                 <li className="nav-item">
+//                 <a className="nav-link" href="#">News</a>
+//                 </li>
+//                 <li className="nav-item">
+//                 <a className="nav-link" href="#">About</a>
+//                 </li>
+//                 <li className="nav-item">
+//                 <a className="nav-link" href="#">Contact</a>
+//                 </li>
+//             </ul>
+//             </div>
+//         </div>
+//         </nav>  
+//       </div>
+//     )
+//   }
+// }
+
+import React from 'react'
+
+export default function Navbar() {
+  return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white">
-        <div className="container-fluid">
-            {/* <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button> */}
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">News</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-            </div>
-        </div>
+          <div className="container-fluid">
+              {/* <a className="navbar-brand" href="#">Navbar</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+              </button> */}
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">News</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+              </div>
+          </div>
         </nav>  
-      </div>
-    )
-  }
+  )
 }
-
