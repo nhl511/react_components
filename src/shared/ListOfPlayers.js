b8d06c6a1e5b3712ac036c30fa59926a184f96ca
bestadvutru <longnguyen5112000@gmail.com>
2022-09-16 10:04:38 +0700

    Components Part 2

---
+++ src/shared/ListOfPlayers.js
@@ -0,0 +1,8 @@
+export const players = [
+    {id: 1, name: 'Critiano Ronaldo', club: 'Manchester United', img:'assets/images/cr.jpg'},
+    {id: 2, name: 'Kante', club: 'PSG', img: 'assets/images/kante.jpg'},
+    {id: 3, name: 'Messi', club: 'PSG', img: 'assets/images/messi.jpg'},
+    {id: 4, name: 'Neymar', club: 'PSG', img: 'assets/images/neymar.jpg'},
+    {id: 5, name: 'Kane', club: 'Tottemham', img: 'assets/images/kane.jpg'},
+    {id: 6, name: 'Haaland', club: 'Manchester City', img: 'assets/images/haaland.jpg'}
+];
